959fa60e8e23cc4b32bd1de2ee63aaf0e96e0c6b
Eron Padilla <2300540@fortecpraiagrande.com.br>
2025-02-18 08:35:40 -0300

    Add files via upload

---
+++ ProjetoJava/ProjetoInicialGit/src/main/java/aplicacaogit/ProjetoInicialGit.java
@@ -0,0 +1,16 @@
+package aplicacaogit;
+import java.util.Scanner;
+
+public class ProjetoInicialGit {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Minha primeira vez fazendo programa! com GitHub");
+        System.out.println("Digite o Primeiro Valor");
+        int num1 = scan.nextInt();
+        System.out.println("Digite o Segundo Valor");
+        int num2 = scan.nextInt();
+        System.out.println("Valor é: " + num1 + num2);
+        
+    }
+}
